6ead4e349e24d5adcbcafc80ab11357ed9e635fc
MattB09 <mattbancroft03@gmail.com>
2021-08-25 12:33:16 +0900

    Created endpoints for reading styles and a style.

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-# from typing import Optional
-from fastapi import FastAPI
+from typing import Optional, List
 
-from models import Style
-from database import Base, SessionLocal,engine
 
-Base.metadata.create_all(engine)
-session = SessionLocal()
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import schemas, crud
+from models import Style, Crag, Prefecture, Base
+from database import SessionLocal, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/styles", response_model=List[schemas.Style])
+def read_styles(db: Session=Depends(get_db)):
+    styles = crud.get_styles(db)
+    return styles
+
+@app.get("/styles/{style_id}", response_model = schemas.Style)
+def read_style(style_id: int, db: Session=Depends(get_db)):
+    style = crud.get_style(db, style_id=style_id)
+    if style is None:
+        raise HTTPException(status_code=404, detail="Style not found")
+    return style
 
 @app.get("/")
 def index():
-    styles = session.query(Style).all()
-    return styles
+    return {"hello": "world"}
 
 
 # @app.get("/items/{item_id}")
